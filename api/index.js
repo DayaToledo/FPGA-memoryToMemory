5c6766af6ef48b55fb4d44ba96c87b8104afc6aa
dayana <dayanabritotoledo_@hotmail.com>
2022-12-26 11:10:21 -0300

    Adjust files to vercel

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import server from '../src/http.js'; 
+import '../src/websocket.js';
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`)); 
--- src/server.js
+++
@@ -1,4 +0,0 @@
-import server from './http.js'; 
-import './websocket.js';
-
-server.listen(3000, () => console.log("Server is running on PORT 3000")); 
