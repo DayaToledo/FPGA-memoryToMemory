0fca8ce92ef9753ae7687fa6f9ea42c59c7d816a
dayana <dayanabritotoledo_@hotmail.com>
2022-12-26 12:36:16 -0300

    Adjust files to vercel

--- src/http.js
+++ src/http.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename).split('src')[0];
 
 import { 
   pageHome, 
@@ -18,10 +18,10 @@ import {
 
 const app = express(); 
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'html');
 
-nunjucks.configure(path.join(__dirname, '/views'), {
+nunjucks.configure(path.join(__dirname, 'src/views'), {
     autoescape: true,
     express: app,
     noCache: true,
