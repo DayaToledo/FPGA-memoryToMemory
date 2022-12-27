71e1939625764451aa21a17aade5bd826bbb1f9e
dayana <dayanabritotoledo_@hotmail.com>
2022-12-27 09:58:32 -0300

    Test dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import server from './src/http.js';
 import './src/websocket.js';
 
 const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`)); 
 
--- public/scripts/env.js
+++ public/scripts/env.js
@@ -1,3 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log('url', process.env.VERCEL_URL);
+console.log('port', process.env.PORT);
+
 const siteUrl = process.env.VERCEL_URL || "http://localhost:3000";
 
 export default siteUrl;
